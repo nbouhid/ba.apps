5756423e9f7c162a81da044155244def3f95644e
martin <martin@ciandt.co>
2012-10-08 15:36:14 -0300

    Base de datos

--- source/db/database.sql
+++ source/db/database.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (x86_64)
 --
--- Host: floyd.cit    Database: bs_apps_ciandt_dev
+-- Host: localhost    Database: bsasapp_dev
 -- ------------------------------------------------------
 -- Server version	5.5.24-0ubuntu0.12.04.1
 
@@ -14,6 +14,111 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barrio`
+--
+
+DROP TABLE IF EXISTS `barrio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barrio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barrio`
+--
+
+LOCK TABLES `barrio` WRITE;
+/*!40000 ALTER TABLE `barrio` DISABLE KEYS */;
+INSERT INTO `barrio` VALUES (1,'caballito'),(2,'flores'),(3,'palermo'),(4,'barrio norte');
+/*!40000 ALTER TABLE `barrio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'odiomas'),(2,'artesanias'),(3,'literatura'),(4,'informatica');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `centros`
+--
+
+DROP TABLE IF EXISTS `centros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `centros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(90) NOT NULL,
+  `atencion_desde` time NOT NULL,
+  `atencion_hasta` time NOT NULL,
+  `direccion` varchar(60) NOT NULL,
+  `telefono` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `centros`
+--
+
+LOCK TABLES `centros` WRITE;
+/*!40000 ALTER TABLE `centros` DISABLE KEYS */;
+INSERT INTO `centros` VALUES (1,'Centro Cultural libre solar','00:00:00','18:00:00','Rivadavia 3897','011545488948494'),(2,'Artesanos de Palermo','08:00:00','19:00:00','Nicaragua 6086','0115454899889'),(3,'Literales del Norte','09:30:00','18:30:00','Uriburu 1254','01154897897899878'),(4,'Artesanos de Palermo','08:00:00','19:00:00','Nicaragua 6086','0115454899889');
+/*!40000 ALTER TABLE `centros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(90) NOT NULL,
+  `barrio_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `centro_id` int(11) NOT NULL,
+  `desde` date NOT NULL,
+  `hasta` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'Alfareria',3,2,2,'2012-11-01','2013-02-28'),(2,'Ingles',1,1,1,'2012-10-17','2013-04-30'),(3,'Litetarura',4,3,3,'2012-12-01','2013-04-28'),(4,'Operador OffiSe',2,4,3,'2012-10-18','2012-12-01');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +129,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-10-02 17:30:07
+-- Dump completed on 2012-10-08 15:30:45
